64176199ff23ed93abd13b5615bf9e9e4394159c
Luiz Gustavo <luizgustavocampos2005@gmail.com>
2024-11-13 13:40:25 -0300

    Adicionando Migrate

--- Flask/meu_site.py
+++ Flask/meu_site.py
@@ -1,26 +1,27 @@
 from flask import Flask
+from flask_migrate import Migrate
 from app.controllers.routes import routes_bp
 from app.extensions import db
-from os import getenv
+import os
 from dotenv import load_dotenv
 
 # Carregar as variáveis do .env
 load_dotenv()
 
-# Acessar a chave secreta
-secret_key = getenv("SECRET_KEY")
+# Acessar a chave secretaos.
+secret_key = os.getenv("SECRET_KEY")
 
 # Acessar os dados do banco de dados
 # Banco Principal
-database_user = getenv("DATABASE_USER")
-database_host = getenv("DATABASE_HOST")
-database_password = getenv("DATABASE_PASSWORD")
-database_name = getenv("DATABASE_NAME")
+database_user = os.getenv("DATABASE_USER")
+database_host = os.getenv("DATABASE_HOST")
+database_password = os.getenv("DATABASE_PASSWORD")
+database_name = os.getenv("DATABASE_NAME")
 # Banco Secundário
-database2_user = getenv("DATABASE2_USER")
-database2_host = getenv("DATABASE2_HOST")
-database2_password = getenv("DATABASE2_PASSWORD")
-database2_name = getenv("DATABASE2_NAME")
+database2_user =os. getenv("DATABASE2_USER")
+database2_host = os.getenv("DATABASE2_HOST")
+database2_password = os.getenv("DATABASE2_PASSWORD")
+database2_name = os.getenv("DATABASE2_NAME")
 
 err = {
     'OperationalError': 'Erro ao conectar ao banco de dados.',
@@ -38,9 +39,15 @@ def create_app():
         app.config['SECRET_KEY'] = secret_key
         app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{database_user}:{database_password}@{database_host}/{database_name}"
         #app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{database2_user}:{database2_password}@{database2_host}/{database2_name}"
+
+        # Pasta de migrações personalizada
+        app.config['MIGRATION_DIR'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app', 'migrations')
         
         # Inicializar o SQLAlchemy com o app
         db.init_app(app)
+
+        #Inicializar o Migrate com o app e db
+        migrate = Migrate(app, db, directory=app.config['MIGRATION_DIR'])
 
         with app.app_context():
             db.create_all()
