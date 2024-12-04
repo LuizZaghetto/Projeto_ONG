09a68188b6f4d2d75eebd1d85fee7aa22057d5ae
Luccas Santos <luccaselias0@gmail.com>
2024-12-03 23:55:43 -0300

    alteracao na exibicao do `avatar` na template `perfil_usuario`, alteracao no `form` do template `registro.html`, alteracoes no `functions.py` e melhorias de logica em `auth_routes.py`

--- Flask/app/controllers/auth_routes.py
+++ Flask/app/controllers/auth_routes.py
@@ -59,107 +59,118 @@ def logout():
 # Página de registro
 
 
-@auth_routes_bp.route("/registro", methods=['GET', 'POST'])
+@auth_routes_bp.route("/registro", methods=['GET'])
 def registro():
     usuarioform = forms.registroForm()
     ONGform = forms.ONGregistroForm()
     if current_user.is_authenticated:
         return redirect(url_for("routes.perfil"))
+    else:
+        return render_template('registro/registro.html', usuarioform=usuarioform, ONGform=ONGform)
     # Verificar qual formulário foi enviado
-    if request.method == 'POST':
-        form_type = request.form.get('form_type')
-
-        if form_type == 'usuario' and usuarioform.validate_on_submit():
-            # Lógica para o formulário de usuário
-            usuario = models.Usuarios.query.filter_by(
-                email=usuarioform.email.data).first()
-            if usuario is None:
-                try:
-                    # Formatar CPF e Telefone no backend
-                    cpf_formatado = func.formatar_cpf(usuarioform.CPF.data)
-                    telefone_formatado = func.formatar_telefone(
-                        usuarioform.telefone.data)
-
-                    # Aplicando hash à senha
-                    senha_hashed = generate_password_hash(
-                        usuarioform.senha.data)
-                
-                    slug_usuario = slugify(usuarioform.nome.data)
-
-                    avatar = usuarioform.avatar.data
-                    avatar_filename = None
-                    if avatar and hasattr(avatar, 'filename'):  # Verifica se é um arquivo válido
-                        avatar_filename = f"{slug_usuario}-{func.gerar_hash(avatar.filename)}.jpg"
-                        avatar_path = os.path.join(current_app.config['UPLOAD_FOLDER'], avatar_filename)
-                        avatar.save(avatar_path)
 
 
-                    usuario = models.Usuarios(
-                        nome=usuarioform.nome.data,
-                        email=usuarioform.email.data,
-                        telefone=telefone_formatado,
-                        data_nasc=usuarioform.data_nasc.data.strftime(
-                            '%Y-%m-%d'),
-                        CPF=cpf_formatado,
-                        senha_hash=senha_hashed,
-                        slug=slug_usuario,
-                        avatar=f"uploads/usuarios/{avatar_filename}" if avatar_filename else None
-                    )
+@auth_routes_bp.route('/registro', methods=['POST'])
+def registro_post():
+    usuarioform = forms.registroForm()
+    ONGform = forms.ONGregistroForm()
+    form_type = request.form.get('form_type')
+    if form_type == 'usuario' and usuarioform.validate_on_submit():
+        # Lógica para o formulário de usuário
+        usuario = models.Usuarios.query.filter_by(
+            email=usuarioform.email.data).first()
+        if usuario is None:
+            try:
+                # Formatar CPF e Telefone no backend
+                cpf_formatado = func.formatar_cpf(usuarioform.CPF.data)
+                telefone_formatado = func.formatar_telefone(
+                    usuarioform.telefone.data)
 
-                    # Adicionando e commitando no banco de dados
-                    db.session.add(usuario)
-                    db.session.commit()
-                    # Concatenando o id no slug
-                    usuario.slug = f"{slug_usuario}-{usuario.ID_usuario}"
+                # Aplicando hash à senha
+                senha_hashed = generate_password_hash(
+                    usuarioform.senha.data)
 
-                    db.session.commit()  # Commit a atualização do slug com ID
+                # SLUG
+                slug_usuario = slugify(usuarioform.nome.data)
 
-                    flash("Registro de usuário realizado com sucesso!", "success")
-                    return redirect(url_for("auth_routes.login"))
-                except Exception as e:
-                    db.session.rollback()  # Reverter mudanças no banco em caso de erro
-                    flash(f"Erro ao registrar o usuário: {e}", "danger")
-            else:
-                flash("Esse e-mail já está registrado.", "warning")
-        elif form_type == 'ong' and ONGform.validate_on_submit():
-            # Lógica para o formulário de ONG
-            ong = models.ONG.query.filter_by(email=ONGform.email.data).first()
-            if ong is None:
-                try:
-                    # Aplicando hash à senha
-                    senha_hashed = generate_password_hash(ONGform.senha.data)
+                # AVATAR
+                avatar = request.form
+                print(avatar)
+                avatar_filename = None
+                # Verifica se é um arquivo válido
+                if avatar:
+                    avatar_f = request.files['avatar']
+                    avatar_filename = f"{slug_usuario}-{func.gerar_hash(avatar_f.filename)}"
+                    avatar_path = os.path.join(
+                        current_app.config['UPLOAD_FOLDER'], avatar_filename)
+                    print(avatar_path)
+                    avatar_f.save(avatar_path)
+                usuario = models.Usuarios(
+                    nome=usuarioform.nome.data,
+                    email=usuarioform.email.data,
+                    telefone=telefone_formatado,
+                    data_nasc=usuarioform.data_nasc.data.strftime(
+                        '%Y-%m-%d'),
+                    CPF=cpf_formatado,
+                    senha_hash=senha_hashed,
+                    slug=slug_usuario,
+                    avatar_path=f"uploads/usuarios/{avatar_filename}" if avatar_filename else None
+                )
 
-                    slug_ong = slugify(ONGform.nome.data)
+                # Adicionando e commitando no banco de dados
+                db.session.add(usuario)
+                db.session.commit()
+                # Concatenando o id no slug
+                usuario.slug = f"{slug_usuario}-{usuario.ID_usuario}"
 
-                    ong = models.ONG(
-                        nome=ONGform.nome.data,
-                        email=ONGform.email.data,
-                        telefone=ONGform.telefone.data,
-                        CEP=ONGform.CEP.data,
-                        endereco=ONGform.endereco.data,
-                        bairro=ONGform.bairro.data,
-                        cidade=ONGform.cidade.data,
-                        UF=ONGform.UF.data,
-                        CNPJ=ONGform.CNPJ.data,
-                        senha_hash=senha_hashed,
-                        slug=slug_ong
-                    )
+                db.session.commit()  # Commit a atualização do slug com ID
 
-                    # Adicionando e commitando no banco de dados
-                    db.session.add(ong)
-                    db.session.commit()
-                    # Concatenando o id no slug
-                    ong.slug = f"{slug_ong}-{ong.ID_ONG}"
-
-                    db.session.commit()  # Commit a atualização do slug com ID
-
-                    flash("Registro de ONG realizado com sucesso!", "success")
-                    return redirect(url_for("auth_routes.login"))
-                except Exception as e:
-                    db.session.rollback()  # Reverter mudanças no banco em caso de erro
-                    flash(f"Erro ao registrar a ONG: {e}", "danger")
-            else:
-                flash("Esse e-mail já está registrado.", "warning")
+                flash("Registro de usuário realizado com sucesso!", "success")
+                # return redirect(url_for("auth_routes.login"))
+            except Exception as e:
+                db.session.rollback()  # Reverter mudanças no banco em caso de erro
+                flash(f"Erro ao registrar o usuário: {e}", "danger")
         else:
-            flash("Erro no formulário, corrija os campos destacados.", "warning")
+            flash("Esse e-mail já está registrado.", "warning")
+    elif form_type == 'ong' and ONGform.validate_on_submit():
+        # Lógica para o formulário de ONG
+        ong = models.ONG.query.filter_by(email=ONGform.email.data).first()
+        if ong is None:
+            try:
+                # Aplicando hash à senha
+                senha_hashed = generate_password_hash(ONGform.senha.data)
+
+                slug_ong = slugify(ONGform.nome.data)
+
+                ong = models.ONG(
+                    nome=ONGform.nome.data,
+                    email=ONGform.email.data,
+                    telefone=ONGform.telefone.data,
+                    CEP=ONGform.CEP.data,
+                    endereco=ONGform.endereco.data,
+                    bairro=ONGform.bairro.data,
+                    cidade=ONGform.cidade.data,
+                    UF=ONGform.UF.data,
+                    CNPJ=ONGform.CNPJ.data,
+                    senha_hash=senha_hashed,
+                    slug=slug_ong
+                )
+
+                # Adicionando e commitando no banco de dados
+                db.session.add(ong)
+                db.session.commit()
+                # Concatenando o id no slug
+                ong.slug = f"{slug_ong}-{ong.ID_ONG}"
+
+                db.session.commit()  # Commit a atualização do slug com ID
+
+                flash("Registro de ONG realizado com sucesso!", "success")
+                return redirect(url_for("auth_routes.login"))
+            except Exception as e:
+                db.session.rollback()  # Reverter mudanças no banco em caso de erro
+                flash(f"Erro ao registrar a ONG: {e}", "danger")
+        else:
+            flash("Esse e-mail já está registrado.", "warning")
+    else:
+        flash("Erro no formulário, corrija os campos destacados.", "warning")
     return render_template('registro/registro.html', usuarioform=usuarioform, ONGform=ONGform)
--- Flask/app/functions.py
+++ Flask/app/functions.py
@@ -1,45 +1,37 @@
 import json
 import re
-import app.models.models as models
-import app.forms.forms as forms 
-from app.extensions import db
-import requests
-import random
-from faker import Faker
 from werkzeug.security import generate_password_hash
-import app.models.models as models
 import hashlib
-import time
+import app.models.models as models
+from app.extensions import db
 
-
-fake = Faker("pt_BR")
 
 def carregar_json(filename):
     try:
         with open(filename, "r", encoding="utf-8") as file:
-                return json.load(file)
+            return json.load(file)
     except FileNotFoundError:
         print(f"Erro: O arquivo '{filename}' não foi encontrado.")
         return []
     except json.JSONDecodeError:
         print(f"Erro: O arquivo '{filename}' não é um JSON válido.")
         return []
-    
-import re
+
 
 def formatar_cpf(cpf):
     # Remove qualquer caractere que não seja número
     cpf = re.sub(r'\D', '', cpf)
-    
+
     # Aplica o formato XXX.XXX.XXX-XX
     if len(cpf) == 11:
         return f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
     return cpf
 
+
 def formatar_telefone(telefone):
     # Remove qualquer caractere que não seja número
     telefone = re.sub(r'\D', '', telefone)
-    
+
     # Aplica a formatação (99) 99999-9999
     if len(telefone) == 11:
         return f"({telefone[:2]}) {telefone[2:7]}-{telefone[7:]}"
@@ -47,46 +39,23 @@ def formatar_telefone(telefone):
         return f"({telefone[:2]}) {telefone[2:6]}-{telefone[6:]}"
     return telefone
 
+
 def atualizar_dados_formatados():
-    usuarios = models.Usuarios.query.all()  # Obtem todos os usuários do banco de dados
+    # Obtem todos os usuários do banco de dados
+    usuarios = models.Usuarios.query.all()
     for usuario in usuarios:
         # Formatar CPF e Telefone se necessário
         usuario.CPF = formatar_cpf(usuario.CPF)
         usuario.telefone = formatar_telefone(usuario.telefone)
-        
+
         # Salvar no banco de dados
         db.session.commit()  # Salva as alterações no banco
 
     print("Dados atualizados com sucesso!")
 
-def criar_usuarios():
-    usuarios = []
-    for _ in range(100):
-        senha = fake.password(length=12, special_chars=True, digits=True, upper_case=True, lower_case=True)
-        senha_hash = generate_password_hash(senha)  # Hashea a senha gerada
-        usuario = models.Usuarios(
-            nome=fake.name(),
-            email=fake.email(),
-            telefone=fake.phone_number(),
-            data_nasc=fake.date_of_birth(minimum_age=18, maximum_age=70),
-            CPF=fake.ssn(),
-            senha_hash=senha_hash,  # Ajuste o campo de senha no seu modelo
-        )
-        print(f"Usuário criado: {usuario.nome}, Email: {usuario.email}, Senha: {senha}")
-        usuarios.append(usuario)
-    return usuarios
 
-def adicionar_usuarios_ao_bd(usuarios):
-    try:
-        db.session.bulk_save_objects(usuarios)
-        db.session.commit()
-        print("100 usuários adicionados com sucesso!")
-    except Exception as e:
-        db.session.rollback()
-        print(f"Erro ao adicionar usuários: {e}")
-
-def gerar_hash(filename):
+def gerar_hash(filename: str):
     """Gera um hash único baseado no nome do arquivo e no timestamp atual."""
-    timestamp = str(time.time()).encode('utf-8')
-    nome_arquivo = filename.encode('utf-8')
-    return hashlib.sha256(nome_arquivo + timestamp).hexdigest()[:15]
+    file = filename.split('.')
+    extension = file[-1]
+    return hashlib.sha256(file[0].encode()).hexdigest()+f'.{extension}'
